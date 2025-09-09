a16f5af60d9e859994c58d99b1e81ff30057ee47
themystic1 <cluckyugo@gmail.com>
2025-09-09 13:16:35 +0100

    push

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -31,7 +31,7 @@ const DashboardHeader = () => {
 
   const { skills, loadingSkills } = useSkills();
 
-  console.log(request_subscriptions);
+  // console.log(request_subscriptions);
 
   const allSkils = skills?.data || [];
 
