f2124f742f0b01f7b5ac94dcdf6e465aff1f0b73
Lucky Chukwujekwu <cluckyugo@gmail.com>
2025-09-20 01:44:12 +0100

    ok

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,7 +4,7 @@ import Pricingg from "@/components/dashboard/pricing";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Payment plans", // will render as "Repairfind Premium Customer | Dashboard"
+  title: "Payment plans",
   description: "Select your Payment plans",
 };
 
