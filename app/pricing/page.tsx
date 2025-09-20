50063a6c8cff7b88eaa5785ed6b91ade323a532b
Lucky Chukwujekwu <cluckyugo@gmail.com>
2025-09-20 01:43:09 +0100

    ok

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,19 +1,6 @@
 import Pricingg from "@/components/dashboard/pricing";
-// import { redirect } from "next/navigation";
-
-// app/dashboard/page.tsx
-// import type { Metadata } from "next";
-
-// export const metadata: Metadata = {
-//   description: "Select your Payment plans",
-// };
 
 const PricingPage = async () => {
-  // const token = await isToken();
-
-  // if (!token?.value) redirect("/login");
-  // console.log(token);
-
   return <Pricingg isUpgrade={false} />;
 };
 
